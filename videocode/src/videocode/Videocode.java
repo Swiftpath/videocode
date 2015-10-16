f5d74a6bb12287d3bf997a9a6d9581dfb51af499
Swiftpath <dsfal.13@gmail.com>
2015-10-16 09:31:08 -0500

    video test
    this is a test for a CS clas

---
+++ videocode/src/videocode/Videocode.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videocode;
+
+/**
+ *
+ * @author Dominic
+ */
+public class Videocode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+    
+}
